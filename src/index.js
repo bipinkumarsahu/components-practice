5548b6d357f63ed5d8bafbb3d9f5be938a01684f
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-04 10:30:29 +0530

    First Comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App =()=>{
-    return <div>Hi There!</div>
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+            <a src="/" className="avatar">
+                <img alt="Avatar"></img>
+            </a>
+            <div className="content">
+                <a class Name="author">Sam</a>
+                <div className="metadata">
+                    <span className="date"> Today at 9.30</span>
+                </div>
+                <div className="text"> Nice post!</div>
+               
+
+            </div>
+            </div>
+            
+           
+        </div>
+    )
 };
 
 ReactDOM.render(<App />, document.querySelector("#root") )
