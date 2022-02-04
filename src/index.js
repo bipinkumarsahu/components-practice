ea879098d9da665cd05b39176bb0ec5c95e42542
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-04 13:28:26 +0530

    Using Faker to populate data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from '@faker-js/faker';
 
 const App =()=>{
     return (
         <div className="ui container comments">
             <div className="comment">
-            <a src="/" className="avatar">
-                <img alt="Avatar"></img>
+            <a  className="avatar">
+                <img src={faker.image.avatar()} alt="Avatar"></img>
             </a>
             <div className="content">
-                <a class Name="author">Sam</a>
+                <a className="author">{faker.name.firstName()}</a>
                 <div className="metadata">
-                    <span className="date"> Today at 9.30</span>
+                    <span className="date"> {faker.date.weekday(true)} {faker.time.recent('abbr')}</span>
                 </div>
-                <div className="text"> Nice post!</div>
+                <div className="text"> {faker.lorem.lines(2)}</div>
                
 
             </div>
