839e3d96c7d8571e763734f8c2837b8a58c2a6de
Bipin Sahu <bipinsahu5@gmail.com>
2022-02-04 23:25:37 +0530

    Component Reuse and Nesting

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from '@faker-js/faker';
 import CommentDetail from "./CommentDetail.js";
+import ApprovalCard from "./ApprovalCard.js";
 
 const App =()=>{
     return (
-        <div className="ui container comments">
+        <div className="ui comments one cards ">
+            <ApprovalCard>Are you sure ?</ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author = {faker.name.firstName()} avatar = {faker.image.avatar()} time = {faker.time.recent('abbr')}/>
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author = {faker.name.firstName()} avatar = {faker.image.avatar()} time = {faker.time.recent('abbr')} />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author = {faker.name.firstName()} avatar = {faker.image.avatar()} time = {faker.time.recent('abbr')} />
+            </ApprovalCard>
             
            
         </div>
